baf7e5dd0a93c2887ffedd3df3b1befcf9827897
GErP83 <gurrka@gmail.com>
2018-02-18 17:38:31 +0100

    remove

--- demo/src/main/java/com/gerp83/slimadaptertest/MainActivity.java
+++ demo/src/main/java/com/gerp83/slimadaptertest/MainActivity.java
@@ -3,13 +3,9 @@ package com.gerp83.slimadaptertest;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-import java.util.ArrayList;
 
 /**
- * Created by GErP83 on 2017. 09. 24..
+ * Created by GErP83
  *
  */
 
